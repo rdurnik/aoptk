3dd5d84417c1fc991cfb2f1124b664316913cccc
rdurnik <rdurnik@gmail.com>
2025-11-07 12:01:48 +0100

    Data structure - dataframe vs file path?

--- src/aoptk/pdf.py
+++ src/aoptk/pdf.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 class PDF:
-    def __init__(self, pdf: pd.DataFrame):
+    def __init__(self, pdf: str):
         self.pdf = pdf
     
-    def __str__(self) -> pd.DataFrame:
+    def __str__(self) -> str:
         return self.pdf
--- src/aoptk/publication.py
+++ src/aoptk/publication.py
@@ -3,12 +3,14 @@ from typing import Optional, Dict
 
 @dataclass
 class Publication:
-    id: str # In the final output, user will need a list of IDs based on which the result was concluded (e.g., liver fibrosis is caused by thioacetamide based on publications 1, 2, 3...)
+    id: str # In the final output, user will need a list of IDs based on which the result was concluded (e.g., liver fibrosis is caused by thioacetamide based on publications 1, 2, 3...).
+    # If user provides their own PDFs, the ID could simply be the filename.
     abstract: str
-    non_abstract_text: str
+    full_text: str
     abbreviations: Optional[Dict[str, str]] = field(default_factory=dict)
     tables: Optional[str] = None
     figures: Optional[str] = None
+    figure_description: Optional[str] = None
 
     def __str__(self) -> str:
         pass # ?
