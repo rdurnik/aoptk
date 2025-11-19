3189038a9d201130acd78b692494de98f2acf3b8
rdurnik <rdurnik@gmail.com>
2025-11-19 14:02:31 +0100

    Cleaning

--- src/aoptk/get_pdf.py
+++ src/aoptk/get_pdf.py
@@ -1,8 +1,6 @@
 from abc import ABC
 from abc import abstractmethod
-
 from aoptk.pdf import PDF
-
 
 class GetPDF(ABC):
     @abstractmethod
--- src/aoptk/pdf_parser.py
+++ src/aoptk/pdf_parser.py
@@ -1,12 +1,13 @@
 from abc import ABC
 from abc import abstractmethod
 from aoptk.get_publication import GetPublication
+from aoptk.publication import Publication
 
 class ParsePDF(ABC, GetPublication):
     @abstractmethod
-    def get_publication(self):
+    def get_publication(self) -> Publication:
         pass
 
     @abstractmethod
-    def _parse_pdf(self, pdf):
+    def _parse_pdf(self, pdf) -> str:
         pass
