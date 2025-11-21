03cec0c6e573d01efe7ab200abd63571427d622e
rdurnik <rdurnik@gmail.com>
2025-11-21 10:37:31 +0100

    Chemical data structure updates

--- src/aoptk/mesh_terms.py
+++ src/aoptk/mesh_terms.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from aoptk.normalize_chemical import NormalizeChemical
+from aoptk.chemical import Chemical
 
 
 class MeshTerms(NormalizeChemical):
@@ -8,7 +9,7 @@ class MeshTerms(NormalizeChemical):
     def __init__(self, mesh_terms: pd.DataFrame):
         self._mesh_terms = mesh_terms
 
-    def normalize_chemical(self, chemical: str) -> str:
+    def normalize_chemical(self, chemical: Chemical) -> Chemical:
         """Normalize a chemical name using MeSH terms."""
         for _, row in self._mesh_terms.iterrows():
             heading = row.loc["heading"]
--- src/aoptk/normalize_chemical.py
+++ src/aoptk/normalize_chemical.py
@@ -7,5 +7,5 @@ class NormalizeChemical(ABC):
     """Abstract base class for chemical name normalization."""
 
     @abstractmethod
-    def normalize_chemical(self) -> Chemical:
+    def normalize_chemical(self, chemical: Chemical) -> Chemical:
         """Normalize a chemical name."""
