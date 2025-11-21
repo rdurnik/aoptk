cfcf65c37ac4e3364ea112c8a0d770516a580507
hechth <helge.hecht@recetox.muni.cz>
2025-11-21 12:53:11 +0100

    format

--- tests/test_chemical.py
+++ tests/test_chemical.py
@@ -7,19 +7,23 @@ def acetaminophen() -> Chemical:
     """Fixture to create acetaminophen chemical."""
     return Chemical("acetaminophen")
 
+
 @pytest.fixture
 def thioacetamide() -> Chemical:
     """Fixture to create thioacetamide chemical."""
     return Chemical("thioacetamide")
 
+
 def test_str(acetaminophen: Chemical):
     """Test __str__ function."""
     assert str(acetaminophen) == "acetaminophen"
+
 
 def test_eq(acetaminophen: Chemical, thioacetamide: Chemical):
     """Testing comparison via __eq__ operator."""
     assert acetaminophen != thioacetamide
     assert acetaminophen == "acetaminophen"
+
 
 def test_name(acetaminophen: Chemical):
     """Test name."""
