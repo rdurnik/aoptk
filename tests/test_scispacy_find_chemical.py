1a868cddaabe3ced23ad774d74acd45d2f741a2a
hechth <helge.hecht@recetox.muni.cz>
2025-11-25 16:12:55 +0100

    modified test skip and moved models to own category

--- tests/test_scispacy_find_chemical.py
+++ tests/test_scispacy_find_chemical.py
@@ -7,26 +7,24 @@ from aoptk.scispacy_find_chemical import ScispacyFindChemical
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 
 
-@pytest.mark.skipif(
+pytestmark = pytest.mark.skipif(
     IN_GITHUB_ACTIONS,
     reason="Skip in Github Actions to save energy consumption (large model download required).",
 )
+
 def test_can_create():
     """Can create ScispacyFindChemical instance."""
-    assert os.getenv("GITHUB_URL") is None
     actual = ScispacyFindChemical()
     assert actual is not None
 
 
 def test_implements_interface():
     """ScispacyFindChemical implements FindChemical interface."""
-    assert os.getenv("GITHUB_URL") is None
     assert isinstance(ScispacyFindChemical(), FindChemical)
 
 
 def test_find_chemical_not_empty():
     """Test that find_chemical method returns a non-empty result."""
-    assert os.getenv("GITHUB_URL") is None
     actual = ScispacyFindChemical().find_chemical("")
     assert actual is not None
 
@@ -40,10 +38,10 @@ def test_find_chemical_not_empty():
             "Thioacetamide, carbon tetrachloride and ethanol were used to induce liver injury.",
             ["thioacetamide", "carbon tetrachloride", "ethanol"],
         ),
+        ('Thioacetamide causes cancer.', ['thioacetamide'])
     ],
 )
 def test_find_chemical_one_chemical(sentence: str, expected: list[str]):
     """Test that find_chemical method finds chemicals in text."""
-    assert os.getenv("GITHUB_URL") is None
     actual = [chem.name for chem in ScispacyFindChemical().find_chemical(sentence)]
     assert actual == expected
