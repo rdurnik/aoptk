44827c736252d2cef3d4b4d149a97ab40d39945b
hechth <helge.hecht@recetox.muni.cz>
2025-11-26 15:55:45 +0100

    consolidated code

--- src/aoptk/europepmc.py
+++ src/aoptk/europepmc.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 from pathlib import Path
 import requests
+from aoptk.abstract import Abstract
 from aoptk.get_pdf import GetPDF
+from aoptk.get_abstract import GetAbstract
 from aoptk.pdf import PDF
 from aoptk.utils import get_pubmed_pdf_url
 
 
-class EuropePMC(GetPDF):
+class EuropePMC(GetPDF, GetAbstract):
     """Class to get PDFs from EuropePMC based on a query."""
 
     page_size = 1000
@@ -30,18 +32,7 @@ class EuropePMC(GetPDF):
         id_list = []
 
         while True:
-            params = {
-                "query": self._query,
-                "format": "json",
-                "pageSize": self.page_size,
-                "cursorMark": cursor_mark,
-                "resultType": "idlist",
-            }
-
-            response = requests.get(url, params=params, timeout=self.timeout)
-            response.raise_for_status()
-            data_europepmc = response.json()
-
+            data_europepmc = self.call_api(cursor_mark, 'idlist')
             results = data_europepmc.get("resultList", {}).get("result", [])
 
             id_list.extend([_get_publication_id(result) for result in results])
@@ -86,6 +77,34 @@ class EuropePMC(GetPDF):
             f.writelines(response.iter_content(chunk_size=8192))
         return PDF(filepath)
 
+    def get_abstracts(self) -> list[Abstract]:
+        """Return abstracts from Europe PMC based on the query."""
+        cursor_mark = "*"
+        all_abstracts: list[Abstract] = []
+
+        while True:
+            json_data = self.call_api(cursor_mark, 'core')
+            results = json_data.get("resultList", {}).get("result", [])
+            
+            all_abstracts.extend([Abstract(record.get("abstractText", "")) for record in results])
+            next_cursor = json_data.get("nextCursorMark")
+            if not results or not next_cursor or next_cursor == cursor_mark:
+                break
+            cursor_mark = next_cursor
+        return all_abstracts
+
+    def call_api(self, cursor_mark, resultType):
+        url = "https://www.ebi.ac.uk/europepmc/webservices/rest/search"
+        params = {
+            "query": self._query,
+            "format": "json",
+            "pageSize": self.page_size,
+            "cursorMark": cursor_mark,
+            "resultType": resultType,
+        }
+        response = requests.get(url, params=params, timeout=self.timeout)
+        response.raise_for_status()
+        return response.json()
 
 def _get_publication_id(result: dict) -> str | None:
     return result.get("pmcid") or result.get("pmid") or result.get("id")
