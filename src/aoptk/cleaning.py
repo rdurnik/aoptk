93c8a1904945ab21de8869cada10f0ba0fd03b6d
rdurnik <rdurnik@gmail.com>
2025-11-28 13:07:14 +0100

    Initial version for Cleaning interface and HTML tag remover

---
+++ src/aoptk/cleaning.py
@@ -0,0 +1,9 @@
+from abc import abstractmethod
+
+
+class CleanText:
+    """Abstract base class for cleaning text."""
+
+    @abstractmethod
+    def clean_text(self, text: str) -> str:
+        """Return a cleaned version of the input text."""
