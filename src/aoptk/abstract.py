f7488a277ac4680f41f3662de8549f3d2b521334
rdurnik <rdurnik@gmail.com>
2025-12-03 17:00:34 +0100

    Initial version, work in progress

--- src/aoptk/abstract.py
+++ src/aoptk/abstract.py
@@ -1,8 +1,11 @@
+from aoptk.id import ID
+
 class Abstract:
     """Abstract data structure containing the text of the abstract."""
 
-    def __init__(self, text: str):
+    def __init__(self, text: str, id: ID):
         self.text = text
+        self.id = id
 
     def __str__(self) -> str:
         return self.text
