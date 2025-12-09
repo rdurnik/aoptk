e16a01b9eb059443ae487283683c32fcb81046f4
hechth <helge.hecht@recetox.muni.cz>
2025-12-09 10:27:27 +0100

    added exception handling

--- src/aoptk/utils.py
+++ src/aoptk/utils.py
@@ -1,4 +1,6 @@
 from metapub import FindIt
+from metapub.exceptions import NoPDFLink
+from requests import HTTPError
 
 
 def is_europepmc_id(publication_id: str) -> bool:
@@ -8,4 +10,7 @@ def is_europepmc_id(publication_id: str) -> bool:
 
 def get_pubmed_pdf_url(publication_id: str) -> str:
     """Get the PubMed PDF URL for a given publication ID."""
-    return FindIt(publication_id, retry_errors=True, max_redirects=5, verify=False).url
+    findit = FindIt(publication_id, retry_errors=True, max_redirects=5, verify=False)
+    if not findit.url:
+        raise NoPDFLink(findit.reason)
+    return findit.url
