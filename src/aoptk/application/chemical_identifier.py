0b1c1665884c28057d11bb5f6d304a8f5b610dec
rdurnik <rdurnik@gmail.com>
2025-12-16 15:07:14 +0100

    Cleaning

--- src/aoptk/application/chemical_identifier.py
+++ src/aoptk/application/chemical_identifier.py
@@ -7,6 +7,7 @@ from aoptk.spacy_processor import Spacy
 from datetime import datetime
 import spacy
 from scispacy.linking import EntityLinker  
+from aoptk.chemical import Chemical
 
 def generate_list_of_relevant_chemicals(use_tg_gates, use_tox21, user_defined_database):
     list_of_relevant_chemicals = []
@@ -20,6 +21,22 @@ def generate_list_of_relevant_chemicals(use_tg_gates, use_tox21, user_defined_da
         relevant_chemicals_database = pd.read_excel(user_defined_database)
         list_of_relevant_chemicals.extend(relevant_chemicals_database['chemical_name'].astype(str).str.lower().unique().tolist())
     return list_of_relevant_chemicals
+
+def find_relevant_chemicals(use_mesh_terms, list_of_relevant_chemicals, chemicals):
+    relevant_chemicals = []
+    for chemical in chemicals:
+        if chemical.name.lower() in list_of_relevant_chemicals:
+            relevant_chemicals.append(chemical.name)
+        elif use_mesh_terms == 'yes':
+            try_to_match_mesh_term_to_relevant_chemical(list_of_relevant_chemicals, relevant_chemicals, chemical)
+    return relevant_chemicals
+
+def try_to_match_mesh_term_to_relevant_chemical(list_of_relevant_chemicals, relevant_chemicals, chemical):
+    if mesh_terms := Spacy().generate_mesh_terms(chemical.name):
+        for term in mesh_terms:
+            if term in list_of_relevant_chemicals:
+                relevant_chemicals.append(term)
+                break
 
 @click.command()
 @click.option('--use_tg_gates', 
@@ -42,7 +59,7 @@ def generate_list_of_relevant_chemicals(use_tg_gates, use_tox21, user_defined_da
 @click.option('--query', 
               type=str, 
               required=True,
-              default='(("liver") AND ("fibrotic" OR "fibrosis") AND ("spheroid*" OR "organoid*" OR "multicellular" OR "coculture*" OR "in vitro model")) AND (("1983"[Date - Entry] : "2025/07/25"[Date - Entry]))',
+              default='("liver") AND ("fibrotic" OR "fibrosis") AND ("spheroid*" OR "organoid*" OR "multicellular" OR "coculture*" OR "in vitro model") AND ("1900/01/01"[PDAT] : "2025/07/25"[PDAT])',
               help='Search term for PubMed or Europe PMC')
 @click.option('--literature_database', 
               type=click.Choice(['pubmed', 'europepmc']), 
@@ -59,44 +76,27 @@ def main(email, use_tg_gates, use_tox21, user_defined_database, query, literatur
         abstracts = PubMed(query).get_abstracts()
     elif literature_database == 'europepmc':
         abstracts = EuropePMC(query).get_abstracts()
-    print(abstracts)
     result_df = pd.DataFrame(columns=['publication_id', 'chemicals', 'relevant_chemicals'])
     for abstract in abstracts:
         id = abstract.publication_id
-        print(abstract)
         chemicals = Spacy().find_chemical(abstract.text)
-        chemicals = [chem for chem in chemicals if chem.name]
-        cleaned_chemicals = []
-        for chem in chemicals:
-            if '-treated' in chem.name.lower() or '-induced' in chem.name.lower():
-                cleaned_name = chem.name.rsplit('-', 1)[0].strip()
-                chem = type(chem)(cleaned_name)
-            cleaned_chemicals.append(chem)
-        chemicals = cleaned_chemicals
-        print([chem.name for chem in chemicals])
-        relevant_chemicals = []
-        for chemical in chemicals:
-            if chemical.name.lower() in list_of_relevant_chemicals:
-                relevant_chemicals.append(chemical.name)
-            elif use_mesh_terms == 'yes':
-                if mesh_terms := Spacy().generate_mesh_terms(chemical.name):
-                    for term in mesh_terms:
-                        if term in list_of_relevant_chemicals:
-                            relevant_chemicals.append(term)
-                            break
+        chemicals = [chem.trimmed_name for chem in chemicals if chem.name]
+        relevant_chemicals = find_relevant_chemicals(use_mesh_terms, list_of_relevant_chemicals, chemicals)
         result_df.loc[len(result_df)] = [id, set([chemical.name for chemical in chemicals]), set(relevant_chemicals)]
     
-    result_df = result_df[result_df['relevant_chemicals'].apply(len) > 0]
-    result_df.to_csv(f'src/aoptk/application/{literature_database}_identified_chemicals_use_mesh_terms_{use_mesh_terms}_{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv', index=False)
+    result_df.to_excel(f'src/aoptk/application/{literature_database}_testing_purposes_use_mesh_terms_{use_mesh_terms}_{datetime.now().strftime("%Y%m%d_%H%M%S")}.xlsx', index=False)
+
+    output_df = result_df[result_df['relevant_chemicals'].apply(len) > 0]
+    output_df.to_excel(f'src/aoptk/application/{literature_database}_identified_chemicals_use_mesh_terms_{use_mesh_terms}_{datetime.now().strftime("%Y%m%d_%H%M%S")}.xlsx', index=False)
 
 
-    result_df = result_df.explode('relevant_chemicals')
-    grouped_df = result_df.groupby('relevant_chemicals').agg(
+    exploded_df = output_df.explode('relevant_chemicals')
+    grouped_df = exploded_df.groupby('relevant_chemicals').agg(
         publication_count=('publication_id', 'count'),
         publication_id=('publication_id', list)
     ).reset_index().sort_values('publication_count', ascending=False)
 
-    grouped_df.to_csv(f'src/aoptk/application/{literature_database}_grouped_chemicals_use_mesh_terms_{use_mesh_terms}{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv', index=False)
+    grouped_df.to_excel(f'src/aoptk/application/{literature_database}_grouped_chemicals_use_mesh_terms_{use_mesh_terms}_{datetime.now().strftime("%Y%m%d_%H%M%S")}.xlsx', index=False)
 
 
 if __name__ == "__main__":
