49e0683782d62a55f91b6d07ad13972e1026d23a
rdurnik <rdurnik@gmail.com>
2026-01-12 12:23:59 +0100

    Lint

--- tests/test_pubmed.py
+++ tests/test_pubmed.py
@@ -47,9 +47,8 @@ def test_raises_query_too_large_error():
 def test_get_id_list():
     """Get publication count returns correct number."""
     actual = PubMed('(hepg2 methotrexate) AND (("2023"[Date - Entry] : "2023"[Date - Entry]))').get_id_list()
-    expected = ['36835489', '37913737', '37891562', '36838959']
+    expected = ["36835489", "37913737", "37891562", "36838959"]
     assert sorted(actual) == sorted(expected)
-
 
 
 @pytest.mark.parametrize(
