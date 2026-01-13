a38aa88843ad823dbd3b2c3bab3532dcb7488088
rdurnik <rdurnik@gmail.com>
2026-01-13 15:46:49 +0100

    Increased max_redirects and request_timeout because get_pubmed_pdf_url often fails due to timeout errors

--- src/aoptk/literature/utils.py
+++ src/aoptk/literature/utils.py
@@ -9,7 +9,7 @@ def is_europepmc_id(publication_id: str) -> bool:
 
 def get_pubmed_pdf_url(publication_id: str) -> str:
     """Get the PubMed PDF URL for a given publication ID."""
-    findit = FindIt(publication_id, retry_errors=True, max_redirects=5, verify=False)
+    findit = FindIt(publication_id, retry_errors=True, max_redirects=10, verify=False, request_timeout=20)
     if not findit.url:
         raise HTTPError(findit.reason)
     return findit.url
