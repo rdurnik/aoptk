86186e74260392dbd7c6c920c46fd513db102c93
rdurnik <rdurnik@gmail.com>
2026-03-11 16:24:10 +0100

    Test clean up

--- tests/test_literature_utils.py
+++ tests/test_literature_utils.py
@@ -1,6 +1,8 @@
 import pytest
 from aoptk.literature.utils import is_europepmc_id
+
 # ruff: noqa: F821
+
 
 @pytest.mark.parametrize(
     ("publication_id", "expected"),
@@ -30,7 +32,8 @@ def test_is_europepmc_id(publication_id: str, expected: bool):
     ],
 )
 @pytest.mark.xfail(
-    reason="PDF not available in Europe PMC. Metapub was removed due to dependency issues.", strict=False,
+    reason="PDF not available in Europe PMC. Metapub was removed due to dependency issues.",
+    strict=False,
 )
 def test_get_pubmed_url(pubmed_id: str, expected: str):
     """Test get pubmed url. Can result in 403 HHTP Error."""
