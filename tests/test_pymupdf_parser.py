494b1dba8658f2994855fc62841b9d13e193f532
rdurnik <rdurnik@gmail.com>
2026-03-23 14:28:42 +0100

    full_text fuzzywuzzy

--- tests/test_pymupdf_parser.py
+++ tests/test_pymupdf_parser.py
@@ -62,7 +62,7 @@ def test_extract_full_text_pmc(publication: dict):
     """Test extracting full text from PMC PDFs."""
     pub = publication["publication"]
     actual = pub.full_text
-    expected = publication["full_text"]
+    expected = publication["full_text"].replace('\n', '')
     ratio = fuzz.ratio(actual, expected)
     assert ratio >= 25
 
