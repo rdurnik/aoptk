cce96910e1ea7181eb16098926412486f187bb95
rdurnik <rdurnik@gmail.com>
2026-03-24 12:09:22 +0100

    full text now comes from the same source as in other places

--- tests/test_pmc.py
+++ tests/test_pmc.py
@@ -2,10 +2,13 @@ from __future__ import annotations
 import shutil
 from pathlib import Path
 import pytest
+from fuzzywuzzy import fuzz
 from requests import HTTPError
 from aoptk.literature.databases.pmc import PMC
 from aoptk.literature.get_pdf import GetPDF
 from aoptk.literature.get_publication import GetPublication
+
+# ruff: noqa: PLR2004
 
 test_figure_storage_dir = "tests/figure_storage"
 
@@ -39,21 +42,15 @@ def test_open_access_pmc_pdf_file_exists():
     shutil.rmtree("tests/storage", ignore_errors=True)
 
 
-@pytest.mark.parametrize(
-    ("query", "full_text_path", "images_number"),
-    [
-        ("PMC12416454", "tests/test-data/PMC12416454.txt", 6),
-    ],
-)
 @pytest.mark.xfail(raises=HTTPError)
-def test_full_text_extraction(query: str, full_text_path: str, images_number: int):
-    """Test that full text can be extracted from an open access PMC PDF."""
-    actual = PMC(query, figure_storage=test_figure_storage_dir).get_publications()
-    with Path.open(full_text_path, encoding="utf-8") as f:
-        expected_full_text = f.read()
-    assert actual[0].full_text == expected_full_text
-    assert len(actual[0].figures) == images_number
-    shutil.rmtree(test_figure_storage_dir, ignore_errors=True)
+def test_extract_full_text(provide_pdfs: dict):
+    """Test extracting full text from XMLs."""
+    actual = PMC(provide_pdfs["id"], figure_storage=test_figure_storage_dir).get_publications()
+    expected = provide_pdfs["full_text"]
+    ratio = fuzz.ratio(actual, expected)
+    assert ratio >= 90
+    if Path(test_figure_storage_dir).exists():
+        shutil.rmtree(test_figure_storage_dir)
 
 
 @pytest.mark.xfail(raises=HTTPError)
