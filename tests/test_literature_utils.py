85094c68a91c3fc2378cafd9dd486fb0632d311b
rdurnik <rdurnik@gmail.com>
2026-03-30 17:21:58 +0200

    removed metapub artefact

--- tests/test_literature_utils.py
+++ tests/test_literature_utils.py
@@ -16,25 +16,3 @@ def test_is_europepmc_id(publication_id: str, expected: bool):
     actual = is_europepmc_id(publication_id)
     assert actual == expected
 
-
-@pytest.mark.parametrize(
-    ("pubmed_id", "expected"),
-    [
-        (
-            "41107038",
-            [
-                "https://gut.bmj.com/content/gutjnl/75/2/326.full.pdf",
-                "https://gut.bmj.com/content/75/2/326.full.pdf",
-                "https://gut.bmj.com/content/gutjnl/early/2025/10/16/gutjnl-2025-336400.full.pdf",
-            ],
-        ),
-        ("26733159", ["https://www.degruyter.com/document/doi/10.1515/hsz-2015-0265/pdf"]),
-    ],
-)
-@pytest.mark.xfail(
-    reason="PDF not available in Europe PMC. Metapub was removed due to dependency issues.",
-    strict=False,
-)
-def test_get_pubmed_url(pubmed_id: str, expected: str):
-    """Test get pubmed url. Can result in 403 HHTP Error."""
-    assert get_pubmed_pdf_url(pubmed_id) in expected
