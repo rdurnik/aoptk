28b50f9807c1851f8dadf33d6fd14513f8c3570c
rdurnik <rdurnik@gmail.com>
2026-04-17 19:11:59 +0200

    readded synonyms in tests

--- tests/test_pubchem_api.py
+++ tests/test_pubchem_api.py
@@ -24,12 +24,12 @@ def test_normalize_chemical_not_empty():
 @pytest.mark.parametrize(
     ("chemical_name", "expected_heading", "synonyms"),
     [
-        ("CCL4", "carbon tetrachloride"),
-        ("MTX", "methotrexate"),
-        ("thioacetamide", "thioacetamide"),
-        ("Thioacetamide", "thioacetamide"),
-        ("somethingnotinpubchem", "somethingnotinpubchem"),
-        ("paracetamol", "acetaminophen"),
+        ("CCL4", "carbon tetrachloride", {"tetrachlormethan"}),
+        ("MTX", "methotrexate", {"methotrexatum"}),
+        ("thioacetamide", "thioacetamide", {"acetimidic acid, thio-"}),
+        ("Thioacetamide", "thioacetamide", {"acetimidic acid, thio-"}),
+        ("somethingnotinpubchem", "somethingnotinpubchem", {""}),
+        ("paracetamol", "acetaminophen", {"acetaminophen"}),
     ],
 )
 def test_normalize_chemical(chemical_name: str, expected_heading: str, synonyms: set[str]):
