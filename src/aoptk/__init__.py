f9102a80df091477d10814c7a5d6eef1e92d396b
rdurnik <rdurnik@gmail.com>
2026-04-24 19:26:09 +0200

    removed desc

--- src/aoptk/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Documentation about aoptk."""
-
-import logging
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__author__ = "Robin Durník"
-__email__ = "rdurnik@gmail.com"
-__version__ = "0.1.0"
