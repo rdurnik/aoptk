91daa171b09a907945ae4b8fc46abee54a804c09
rdurnik <rdurnik@gmail.com>
2026-05-26 11:22:45 +0200

    updated metadata for pmc

--- src/aoptk/literature/publication_metadata.py
+++ src/aoptk/literature/publication_metadata.py
@@ -1,19 +1,34 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from datetime import datetime
+from aoptk.literature.id import DOI
 from aoptk.literature.id import ID
+from aoptk.literature.id import PMCID
+from aoptk.literature.id import PMID
 
 
 @dataclass
 class PublicationMetadata:
-    """Data structure representing a publication."""
+    """Data structure representing a publication.
+
+    Attributes:
+        id (ID): Identifier by which the publication was found. Can be PMID, PMCID, DOI, or other such as PPR.
+        pmcid (PMCID | None): PubMed Central ID, if available.
+        pmid (PMID | None): PubMed ID, if available.
+        doi (DOI | None): Digital Object Identifier, if available.
+        year (str | None): Year of publication.
+        title (str | None): Title of the publication.
+        authors (list[str] | None): Authors of the publication.
+        journal (str | None): Journal in which the publication was published.
+    """
 
     id: ID
-    publication_date: str
-    title: str
-    authors: str
-    database: str
-    search_date: datetime
+    pmcid: PMCID | None = None
+    pmid: PMID | None = None
+    doi: DOI | None = None
+    year: int | None = None
+    title: str | None = None
+    authors: list[str] | None = None
+    journal: str | None = None
 
     def __str__(self) -> str:
         return self.title
