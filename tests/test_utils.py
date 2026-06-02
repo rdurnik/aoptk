e327562a0f60fa009ba5d65a54057eb6ec4f7a9e
rdurnik <rdurnik@gmail.com>
2026-06-02 09:09:16 +0200

    added parametrization to image conversion test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,15 +6,31 @@ from aoptk.literature.utils import convert_image_format
 from aoptk.literature.utils import remove_pmc_prefix
 
 
-def test_convert_image_format(tmp_path: Path):
-    """Test converting images to a specified format."""
+@pytest.mark.parametrize(
+    ("image_paths", "expected_paths", "expected_size"),
+    [
+        ([Path("tests/test_data/test_figures/gjic.jpeg")], [Path("tests/test_data/test_figures/gjic.png")], 139459),
+        (
+            [Path("tests/test_data/test_figures/liver_fibrosis.png")],
+            [Path("tests/test_data/test_figures/liver_fibrosis.png")],
+            410773,
+        ),
+        (
+            [Path("tests/test_data/test_figures/liver_fibrosis.png"), Path("tests/test_data/test_figures/gjic.jpeg")],
+            [Path("tests/test_data/test_figures/liver_fibrosis.png"), Path("tests/test_data/test_figures/gjic.png")],
+            550232,
+        ),
+    ],
+)
+def test_convert_image_format(tmp_path: Path, image_paths: list[Path], expected_paths: list[Path], expected_size: int):
+    """Test converting images to a specified format using list inputs."""
     shutil.copytree(Path("tests/test_data/test_figures"), tmp_path, dirs_exist_ok=True)
-    actual = convert_image_format(list(tmp_path.iterdir()), target_format="png")
-    expected = [tmp_path / "gjic.png", tmp_path / "liver_fibrosis.png"]
-    total_size = sum(f.stat().st_size for f in tmp_path.iterdir())
-    expected_size = 1375641
-    assert actual == sorted(expected)
-    assert total_size == pytest.approx(expected_size, rel=0.1)
+    images_to_convert = [tmp_path / p.name for p in image_paths]
+    actual = convert_image_format(images_to_convert, target_format="png")
+    expected_image_paths = [tmp_path / p.name for p in expected_paths]
+    actual_size = sum(f.stat().st_size for f in expected_image_paths)
+    assert sorted(actual) == sorted(expected_image_paths)
+    assert actual_size == pytest.approx(expected_size, rel=0.1)
 
 
 def test_remove_pmc_prefix():
