4a8423aa803829228a08074f0766ac64acebb12e
rdurnik <rdurnik@gmail.com>
2026-06-17 10:21:18 +0200

    updated aop-wiki tests

--- tests/test_aop_wiki.py
+++ tests/test_aop_wiki.py
@@ -20,7 +20,7 @@ def test_get_abstracts():
     abstracts = AOPWiki().get_abstracts()
     aop_38 = next(abstract for abstract in abstracts if abstract.id == ID("https://identifiers.org/aop/38"))
     min_length_of_abstract = 2000
-    min_number_of_abstracts = 500
+    min_number_of_abstracts = 300
     assert aop_38.text.startswith("Protein Alkylation leading to Liver Fibrosis")
     assert len(aop_38.text) > min_length_of_abstract
     assert len(abstracts) > min_number_of_abstracts
